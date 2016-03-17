32523d0f907d27682ca853a1d54c7e4337001cc6
MaHoi Hei <alvar@MaHoideMacBook-Pro.local>
2016-03-17 14:07:22 +0000

    doing crazy experiment

--- main5.m
+++ main5.m
@@ -2,10 +2,10 @@ load planes.mat
 figure(13)
 clf
 p = [
-    planes_intersect_point(planes(3,:), planes(9,:), planes(2,:));
-planes_intersect_point(planes(2,:), planes(7,:), planes(6,:));
-planes_intersect_point(planes(6,:), planes(4,:), planes(5,:));
-planes_intersect_point(planes(5,:), planes(8,:), planes(3,:));
+    planes_intersect_point(planes(8,:), planes(6,:), planes(9,:));
+planes_intersect_point(planes(9,:), planes(6,:), planes(7,:));
+planes_intersect_point(planes(7,:), planes(6,:), planes(3,:));
+planes_intersect_point(planes(3,:), planes(6,:), planes(8,:));
 % planes_intersect_point(planes(9,:), planes(5,:), planes(8,:));
 % planes_intersect_point(planes(8,:), planes(5,:), planes(6,:));
 % planes_intersect_point(planes(6,:), planes(5,:), planes(7,:));
